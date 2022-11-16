64020981bae5627b5c48a8d1f6422b48e68a16a1
Tony Shen <shenguangqing2008@gmail.com>
2022-11-16 12:49:28 -0700

    Added use case

--- com.pokemonbattle/src/Main.java
+++ com.pokemonbattle/src/Main.java
@@ -1,3 +1,15 @@
+/*
+Pokemon Battle Simulator
+Use case:
+    1. Start a new game
+    2. Generate player1 and player2
+    3. Six pokemons are generated for each player
+    4. Each player select a pokemon for each round
+    5. Pokemons battle automatically
+    6. Repeat step 4 ~ 5 for another 5 rounds
+    7. End game - one player wins or draw
+ */
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
